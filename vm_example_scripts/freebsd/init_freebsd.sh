e239753959ad13e728ba009fc6f16e9490c7ac22
victor <victor@brutalix.org>
2024-08-11 23:44:47 +0200

    Freebsd init script

---
+++ vm_example_scripts/freebsd/init_freebsd.sh
@@ -0,0 +1,30 @@
+#!/bin/csh
+
+# Variables
+set IP = "$1"
+set HOSTNAME = "$2"
+set IP_RANGE = `echo "$IP" | sed 's/\.[0-9]*$//'`
+set IP_GATEWAY = "1"
+set IP_NETMASK = "255.255.255.0"
+
+# Jail related config (assuming you need these)
+sysrc jail_enabled="YES"
+sysrc cloned_interfaces="lagg0"
+echo ".include "/etc/jail.conf.d/*.conf";" > /etc/jails.conf
+
+# Set hostname
+sysrc hostname="$HOSTNAME.local"
+
+# Set IP
+sysrc ifconfig_em0="inet $IP netmask $IP_NETMASK broadcast $IP_RANGE.255"
+sysrc defaultrouter="$IP_RANGE.$IP_GATEWAY"
+
+# Set DNS
+cat > /etc/resolv.conf << EOF
+nameserver 208.67.222.222
+nameserver 208.67.220.220
+EOF
+
+# Restart network
+service netif restart
+service routing restart
